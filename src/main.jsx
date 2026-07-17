0bcc6ec8428bc528b97b33f8c1cc11720658279a
refhalman <reefmhalman11@icloud.com>
2026-07-17 22:46:13 +0930

    Add src/main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import MomentumApp from "./MomentumApp.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <MomentumApp />
+  </React.StrictMode>
+);
